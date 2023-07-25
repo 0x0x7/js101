8ebe6c33b734b31066eec3e0232d967428f22eb1
soghaatol <imghaatol@gmail.com>
2023-07-25 02:09:16 +0200

    add bonus features

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -15,43 +15,49 @@ function invalidNumber(number) {
 prompt("Welcome to the calculator");
 let readline = require('readline-sync');
 
-prompt("enter the first number");
-let firstNum = readline.question();
+while (true) {
+  prompt("enter the first number");
+  let firstNum = readline.question();
 
-while (invalidNumber(firstNum)) {
-  prompt("Please enter a valid number");
-  firstNum = readline.question();
+  while (invalidNumber(firstNum)) {
+    prompt("Please enter a valid number");
+    firstNum = readline.question();
+  }
+
+  prompt("enter the second number");
+  let secondNum = readline.question();
+
+  while (invalidNumber(secondNum)) {
+    prompt("Please enter a valid number");
+    secondNum = readline.question();
+  }
+
+  prompt("what operation would you perform? select a number:\n1) add 2) multiply 3) subtract 4) divide");
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Enter 1, 2, 3 or 4');
+    operation = readline.question();
+  }
+
+  let result;
+  switch (operation) {
+    case '1':
+      result = Number(firstNum) + Number(secondNum);
+      break;
+    case '2':
+      result = Number(firstNum) * Number(secondNum);
+      break;
+    case '3':
+      result = Number(firstNum) - Number(secondNum);
+      break;
+    case '4':
+      result = Number(firstNum) / Number(secondNum);
+      break;
+  }
+  prompt(`the result is ${result}`);
+  prompt('Would you like to perform another operation? (y/n)');
+  let answer = readline.question();
+
+  if (answer !== 'y') break;
 }
-
-prompt("enter the second number");
-let secondNum = readline.question();
-
-while (invalidNumber(secondNum)) {
-  prompt("Please enter a valid number");
-  secondNum = readline.question();
-}
-
-prompt("what operation would you perform? select a number:\n1) add 2) multiply 3) subtract 4) divide");
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Enter 1, 2, 3 or 4');
-  operation = readline.question();
-}
-
-let result;
-switch (operation) {
-  case '1':
-    result = Number(firstNum) + Number(secondNum);
-    break;
-  case '2':
-    result = Number(firstNum) * Number(secondNum);
-    break;
-  case '3':
-    result = Number(firstNum) - Number(secondNum);
-    break;
-  case '4':
-    result = Number(firstNum) / Number(secondNum);
-    break;
-}
-prompt(`the result is ${result}`);
