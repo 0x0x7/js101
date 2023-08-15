f43bff0b7d7d7df8c85ed7d3e51b5787c27265ec
soghaatol <imghaatol@gmail.com>
2023-08-15 04:10:18 +0200

    minor refactor

--- easy1_problems/roomSize.js
+++ easy1_problems/roomSize.js
@@ -6,14 +6,20 @@ Enter the width of the room in meters:
 The area of the room is 70.00 square meters (753.47 square feet).
 */
 
+// Import readline-sync library
 let readLineSync = require('readline-sync');
 
 const SQFT = 10.7639;
 
+// Require length:
 let length = readLineSync.question("What's the length of your room in meters: ");
+
+// Require width:
 let width = readLineSync.question("What's the width of your room in meters: ");
 
-let areaSqm = length * width;
-let areaSqft = areaSqm * SQFT;
+// Do calculation
+let sqm = length * width;
+let sqft = sqm * SQFT;
 
-console.log(`Your room is ${areaSqm.toFixed(2)} square meters, or (${areaSqft.toFixed(2)} square feet).`);
+// Log it
+console.log(`Your room is ${sqm.toFixed(2)} square meters, or (${sqft.toFixed(2)} square feet).`);
