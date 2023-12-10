836eacac57fa207a4b51166f7c6c65b51b19c929
soghaatol <imghaatol@gmail.com>
2023-12-10 21:09:48 +0100

    x

--- lesson_3/Easy2.js
+++ lesson_3/Easy2.js
@@ -28,11 +28,11 @@ console.log(numbers); // [ 5, 4, 3, 2, 1 ]
 // Solution:
 
 let numbers = [1, 2, 3, 4, 5];
-let reversedArray = numbers.slice().reverse();
-console.log(reversedArray); // [5, 4, 3, 2, 1]
+console.log(numbers.slice().reverse()); // [5, 4, 3, 2, 1]
 console.log(numbers); // [1, 2, 3, 4, 5]
 
+// Using the spread syntax to sort the array in descending order:
+
 let numbers = [1, 2, 3, 4, 5];
-let sortedArray = [...numbers].sort((num1, num2) => num2 - num1);
-console.log(sortedArray); // [5, 4, 3, 2, 1]
+console.log([...numbers].sort((num1, num2) => num2 - num1); // [5, 4, 3, 2, 1]
 console.log(numbers); // [1, 2, 3, 4, 5]
