fd57ed2f09616f94a821f15fe1661675196b4fed
soghaatol <imghaatol@gmail.com>
2023-12-13 23:54:13 +0100

    x

--- lesson_3/Easy2.js
+++ lesson_3/Easy2.js
@@ -68,3 +68,20 @@ console.log(famousWords.concat("Four score and ")); // 'seven years ago...Four s
 let x = [1, 2, 3, 4, 5];
 x.splice(2, 1);
 console.log(x); // [1, 2, 4, 5]
+
+// Question 6: Suppose we build an array like this:
+
+let flintstones = ["Fred", "Wilma"];
+flintstones.push(["Barney", "Betty"]);
+flintstones.push(["Bambam", "Pebbles"]);
+
+// This code will create a nested array that looks like this:
+
+["Fred", "Wilma", ["Barney", "Betty"], ["Bambam", "Pebbles"]];
+
+// Nesting data structures like we do here is commonplace in JavaScript and programming
+// in general. We'll explore this in much greater depth in a future Lesson.
+
+// Create a new array that contains all of the above values, but in an un-nested format:
+
+[ 'Fred', 'Wilma', 'Barney', 'Betty', 'Bambam', 'Pebbles' ]
