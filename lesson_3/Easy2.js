8e4480121687749edc22b35bbb903941d39ae205
soghaatol <imghaatol@gmail.com>
2023-12-13 23:56:17 +0100

    x

--- lesson_3/Easy2.js
+++ lesson_3/Easy2.js
@@ -85,3 +85,7 @@ flintstones.push(["Bambam", "Pebbles"]);
 // Create a new array that contains all of the above values, but in an un-nested format:
 
 [ 'Fred', 'Wilma', 'Barney', 'Betty', 'Bambam', 'Pebbles' ]
+
+// Solution:
+
+// Using concat() is one way to do this:
